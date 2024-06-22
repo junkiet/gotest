b1f87a8b3a1a8a80e7cd164e2bbc1c3ca8bd0f1d
Jkey <kongjunkiet@gmail.com>
2024-06-23 01:47:10 +0800

    update

--- db/Connect.go
+++ db/Connect.go
@@ -5,6 +5,7 @@ import (
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 	"gorm.io/gorm/schema"
 )
 
@@ -17,11 +18,15 @@ func InitDB() {
 	port := 3306
 	dbname := "wm"
 	timeout := "10s"
+
+	var mysqlLogger logger.Interface = logger.Default.LogMode(logger.Info)
+
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&timeout=%s",
 		username, password, host, port, dbname, timeout)
 
 	var err error
 	DBConnect, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		Logger: mysqlLogger,
 		NamingStrategy: schema.NamingStrategy{
 			TablePrefix:   "jk_",
 			SingularTable: false,
