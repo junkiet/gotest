cf9809c7c2895bef1daf6b25d2156c351d1db27f
Jkey <kongjunkiet@gmail.com>
2024-06-25 05:46:37 +0800

    update

--- res/index.go
+++ res/index.go
@@ -15,13 +15,6 @@ func OK(c *gin.Context, data any) {
 	})
 }
 
-func OKWithMsg(c *gin.Context, msg string) {
-	c.JSON(200, Response{
-		Code: 200,
-		Msg:  msg,
-	})
-}
-
 func OKWithData(c *gin.Context, data any) {
 	c.JSON(200, Response{
 		Code: 200,
@@ -30,72 +23,57 @@ func OKWithData(c *gin.Context, data any) {
 	})
 }
 
-func BadRequest(c *gin.Context, msg string) {
+func BadRequest(c *gin.Context, data any) {
 	c.JSON(400, Response{
 		Code: 400,
-		Msg:  msg,
+		Msg:  "failed",
+		Data: data,
 	})
 }
 
-func Unauthorized(c *gin.Context, msg string) {
+func Unauthorized(c *gin.Context, data any) {
 	c.JSON(401, Response{
 		Code: 401,
-		Msg:  msg,
+		Msg:  "failed",
+		Data: data,
 	})
 }
 
-func Forbidden(c *gin.Context, msg string) {
+func Forbidden(c *gin.Context, data any) {
 	c.JSON(403, Response{
 		Code: 403,
-		Msg:  msg,
+		Msg:  "failed",
+		Data: data,
 	})
 }
 
-func NotFound(c *gin.Context, msg string) {
+func NotFound(c *gin.Context, data any) {
 	c.JSON(404, Response{
 		Code: 404,
-		Msg:  msg,
+		Msg:  "failed",
+		Data: data,
 	})
 }
 
-func InternalServerError(c *gin.Context, msg string) {
+func InternalServerError(c *gin.Context, data any) {
 	c.JSON(500, Response{
 		Code: 500,
-		Msg:  msg,
+		Msg:  "failed",
+		Data: data,
 	})
 }
 
-func ServiceUnavailable(c *gin.Context, msg string) {
+func ServiceUnavailable(c *gin.Context, data any) {
 	c.JSON(503, Response{
 		Code: 503,
-		Msg:  msg,
+		Msg:  "failed",
+		Data: data,
 	})
 }
 
-func TooManyRequests(c *gin.Context, msg string) {
+func TooManyRequests(c *gin.Context) {
 	c.JSON(429, Response{
 		Code: 429,
-		Msg:  msg,
-	})
-}
-
-func Conflict(c *gin.Context, msg string) {
-	c.JSON(409, Response{
-		Code: 409,
-		Msg:  msg,
-	})
-}
-
-func Gone(c *gin.Context, msg string) {
-	c.JSON(410, Response{
-		Code: 410,
-		Msg:  msg,
-	})
-}
-
-func PreconditionFailed(c *gin.Context, msg string) {
-	c.JSON(412, Response{
-		Code: 412,
-		Msg:  msg,
+		Msg:  "failed",
 	})
 }
